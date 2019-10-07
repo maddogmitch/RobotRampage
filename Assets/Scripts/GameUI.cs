cb126ac5da1a83e2d7e6d63959d82abf574db525
maddogmitch <mightymitch66@gmail.com>
2019-10-07 17:39:08 -0400

    Reticle

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    //Allows infor to be conveyed in the runtime
+    [SerializeField]
+    //Represents an imported texture
+    Sprite redReticle;
+    [SerializeField]
+    Sprite yellowReticle;
+    [SerializeField]
+    Sprite blueReticle;
+    [SerializeField]
+    Image reticle;
+
+    public void UpdateRecticle()
+    {
+        //Changes the sprite to reflect the active gun
+        switch(GunEquipper.activeWeaponType)
+        {
+            case Constants.Pistol:
+                reticle.sprite = redReticle;
+                break;
+            case Constants.Shotgun:
+                reticle.sprite = yellowReticle;
+                break;
+            case Constants.AssaultRifle:
+                reticle.sprite = blueReticle;
+                break;
+            default:
+                return;
+        }
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
