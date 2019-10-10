09a71d4cdec8bc001a506e8efe71963e70920e6c
maddogmitch <mightymitch66@gmail.com>
2019-10-10 16:53:46 -0400

    Animating Enemies

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    [SerializeField]
+    //Is the type of robot
+    private string robotType;
+
+    public int health;
+    public int range;
+    public float fireRate;
+
+    public Transform missleFireSpot;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    private Transform player;
+    private float timeLastFired;
+
+    private bool isDead;
+
+    public Animator robot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //
+        isDead = false;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDead)
+        {
+            return;
+        }
+
+        transform.LookAt(player);
+
+        agent.SetDestination(player.position);
+
+        if(Vector3.Distance(transform.position, player.position) < range && Time.time - timeLastFired > fireRate)
+        {
+            timeLastFired = Time.time;
+            fire();
+        }
+    }
+
+    private void fire()
+    {
+        robot.Play("Fire");
+    }
+}
